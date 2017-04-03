57391876bdf58e664d6a6be9cd0109ebea5bf50a
jonathan murphy <jonathan@jmurphy.me>
2017-04-03 15:00:08 -0400

    Updated evaluator, barbarian

--- evaluator.rkt
+++ evaluator.rkt
@@ -3,28 +3,19 @@
 ; Csv viewing functions
 ; Copied from csv article
 ; docs.racket-lang.org/csv-reading/index.html
-(define make-food-csv-reader
-  (make-csv-reader-maker
-   '((separator-chars            #\|)
-     (strip-leading-whitespace?  . #t)
-     (strip-trailing-whitespace? . #t))))
-     
-(define (next-row path)
-  (make-food-csv-reader (open-input-file path)))
+;(define make-food-csv-reader
+;  (make-csv-reader-maker
+;   '((separator-chars            #\|)
+;     (strip-leading-whitespace?  . #t)
+;     (strip-trailing-whitespace? . #t))))
+;     
+;(define (next-row path)
+;  (make-food-csv-reader (open-input-file path)))
 
 
 ; Static Vars
 (define nil '())
 (define folder-base "DND")
-
-; Character Vars
-(define CharClassName nil)
-(define CharSubClassName nil)
-(define CharStatList nil)
-(define CharSkillList nil)
-(define Hitdice nil)
-(define CharInventory nil)
-(define CharNotes nil)
 
 ; Relevant Structs
 (define-struct Stat-Struct (Num IsSavingThrow) #:transparent)
@@ -33,13 +24,11 @@
 ; Generate Stats List
 (define (generatestats)
   (define stats-file (string-append folder-base "/stats.csv"))
-  (define lines (next-row stats-file))
-  (define roll-string (car (lines)))
-  (define list-stats (car (csv->list (car (lines)))))
-  (build-list (length list-stats)
-              (lambda (n)
-                (cons (list-ref list-stats n)
-                      (make-Stat-Struct (evaluator (list roll-string)) #f))))
+  (define stats-roll (car (csv->list (open-input-file (string-append folder-base "/statsroll.csv")))))
+  (csv-map (lambda (n)
+              (cons
+               (car n)
+               (make-Stat-Struct (evaluator stats-roll) #f))) (open-input-file stats-file))
   )
 
 ; Generate Skill List
@@ -56,18 +45,40 @@
 ; Assumes exp is a list
 (define (evaluator exp)
   (cond
-    [(diceroll? (car exp)) (diceroll (car exp))]
-    [else #f]))
+    [(diceroll? exp) (diceroll exp)]
+    [else exp]))
+
+; Returns true if exp1 is a list and matches exp2
+; Taken from evaluator hw
+(define (if-exp-match? exp1 exp2)
+  (if (list? exp1)
+      (eq? (car exp1) exp2)
+      #f))
+
+; Add functions
+(define (add:? exp)
+  (if-exp-match? exp))
+
+(define (add:+ exp)
+  (if (or (null? exp) (null? (cdr exp)))
+      0
+      (+ (evaluator (cadr exp)) (add:+ (cddr exp)))))
+
 
 ; Diceroll?
 (define (diceroll? dr)
-  (if (string? dr)
-      (regexp-match? #px"[0-9]+d[0-9]+" dr)
-      #f))
+  (begin
+    (define dir 0)
+    (if (list? dr)
+        (set! dir (car dr))
+        (set! dir dr))
+    (if (string? dir )
+        (regexp-match? #px"[0-9]+d[0-9]+"  dir)
+        #f)))
 
 (define (diceroll roll-string)
   (define numdice (car (regexp-match #px"[0-9]+" roll-string)))
-  (define dicesize (car (regexp-match #px"[0-9]+" (car (regexp-match #px"d[0-9]+" roll-string)))))
+  (define dicesize (car (regexp-match #px"[0-9]+" (car (regexp-match #px"d[0-9]+"  roll-string)))))
   (roll (string->number numdice) (string->number dicesize))
   )
 
