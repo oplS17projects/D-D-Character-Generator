9c0a0c74c02253212fb4ea4a1fbe4c3441bc75a7
a <van_ha@student.uml.edu>
2017-04-07 10:20:45 -0400

    added tabs

--- main.rkt
+++ main.rkt
@@ -5,6 +5,13 @@
 
 (define logo (read-bitmap "./DND/ddlogo.png"))
 
+; Johnny Douglas' Dungeons & Dragons cartoon theme
+; converted from youtube video
+; https://www.youtube.com/watch?v=v2u7-M7Ouok&t=37s
+(define theme (rs-read "./DND/DDTheme.wav"))
+(play theme)
+
+
 ; hash table to contain stats
 (define stats (make-hash))
 
@@ -15,6 +22,9 @@
 ; sets key/value pair in hash table
 (define (setStat key val)
   (hash-set! stats key val))
+
+; variable to show main screen
+(define mainOn #t)
 
 ; sets race in stats hash table based on radiobox choice
 (define (setRace x)
@@ -34,33 +44,68 @@
                   [width 560]
                   [height 300]))
 
+; panel for logo
 (define logoPanel (new horizontal-panel% [parent main]
                        [min-height 72]))
+
+; panels for attributes and character generation
 (define choicePanel (new horizontal-panel% [parent main]
                          [min-height 200]))
 (define genPanel (new horizontal-panel% [parent main]
                       [alignment '(center center)]))
 
+;draws logo
 (new canvas% [parent logoPanel]
     [style '(control-border)]
     [paint-callback
      (λ (canvas dc)
      (send dc draw-bitmap logo 0 0))])
 
-(define leftPanel (new vertical-panel% [parent choicePanel]))
-(define rightPanel (new vertical-panel% [parent choicePanel]))
+; tabs for various attribute choices
+(define tab (new tab-panel%
+                 [parent choicePanel]
+                 [choices (list "Names"
+                                "Race"
+                                "Class"
+                                "Base Stats")]
+                 [callback (λ (b e)
+                             (case (send b get-selection)
+                               ((0) (send b change-children (λ (children) (list name-panel))))
+                               ((1) (send b change-children (λ (children) (list race-panel))))
+                               ((2) (send b change-children (λ (children) (list class-panel))))
+                               ((3) (send b change-children (λ (children) (list stats-panel))))
+                               ))]))
 
+; panels for each attribute tab 
+(define name-panel (new panel% [parent tab]))
+(define race-panel (new panel% [parent tab]
+                        [style '(deleted)]))
+(define class-panel (new panel% [parent tab]
+                         [style '(deleted)]))
+(define stats-panel (new panel% [parent tab]
+                         [style '(deleted)]))
+
+; text boxes for character and player names tab
+(define align-name-panel (new vertical-panel% [parent name-panel]))
 (define playerName (new text-field%
                         [label "Player Name"]
-                        [parent leftPanel]
+                        [parent align-name-panel]
                         [vert-margin 4]
                         [callback (λ (b e)
                                     (setStat "Player Name" (send playerName get-value)))]))
 
+(define charName (new text-field%
+                        [label "Character Name"]
+                        [parent align-name-panel]
+                        [vert-margin 4]
+                        [callback (λ (b e)
+                                  (setStat "Character Name" (send charName get-value)))]))
+
+; choices for race tab
 (define raceBox (new radio-box%
                      [label "Race"]
                      [choices (list "Human" "Elf" "Dwarf")]
-                     [parent leftPanel]
+                     [parent race-panel]
                      [vert-margin 10]
                      [horiz-margin 5]
                      [style (list 'vertical 'vertical-label)]
@@ -68,30 +113,22 @@
                                (setRace (send raceBox get-selection)))]
                      ))
 
-(define charName (new text-field%
-                        [label "Character Name"]
-                        [parent rightPanel]
-                        [vert-margin 4]
-                        [callback (λ (b e)
-                                  (setStat "Character Name" (send charName get-value)))]))
+; choices for class tab
 (define classBox (new radio-box%
                      [label "Class"]
                      [choices (list "Barbarian" "Cleric" "Mage")]
-                     [parent rightPanel]
+                     [parent class-panel]
                      [style (list 'vertical 'vertical-label)]
                      [callback (λ (b e)
                                (setClass (send classBox get-selection)))]
                      ))
 
+; character sheet generation button
 (define genSheet (new button%
                       [label "Generate Character Sheet"]
                       [parent genPanel]
                       [callback (λ  (b e)
                                   (genCS #t))]))
-(send main show #t)
 
-; Johnny Douglas' Dungeons & Dragons cartoon theme
-; converted from youtube video
-; https://www.youtube.com/watch?v=v2u7-M7Ouok&t=37s
-(define theme (rs-read "./DND/DDTheme.wav"))
-(play theme)
+; displays main window
+(send main show mainOn)
